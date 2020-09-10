508c101cfe43f373dd404afa41a626323f4b16ac
BrandonWalker88 <Brandon.Walker2008@gmail.com>
2020-09-09 22:01:50 -0400

    attempted to add timer and score card

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,33 +6,47 @@ var questions = [
     c: "good",
   },
   {
-    q: "how is your day",
+    q: "how was work?",
     a: ["good", "bad", "ok", "not ok"],
-    c: "good",
+    c: "not ok",
   },
   {
-    q: "how is your day",
+    q: "how is life",
     a: ["good", "bad", "ok", "not ok"],
-    c: "good",
+    c: "ok",
   },
   {
-    q: "how is your day",
+    q: "how is your dog",
     a: ["good", "bad", "ok", "not ok"],
-    c: "good",
+    c: "bad",
+  },
+  {
+    q: "how is your cat",
+    a: ["good", "bad", "ok", "not ok"],
+    c: "ok",
+  },
+  {
+    q: "how is your fish",
+    a: ["good", "bad", "ok", "not ok"],
+    c: "not ok",
   },
 ];
-var counter = 0;
+var correctCounter = 0;
+var questionCounter = 0;
 var cardBody = document.getElementById("questions");
-
+var timeEl = document.getElementById("secondsLeft");
+var secondsLeft = 60;
+var scoreEl = 0;
 function showQuestions() {
+  cardBody.innerHTML = "";
   var question = document.createElement("p");
-  question.textContent = questions[counter].q;
+  question.textContent = questions[questionCounter].q;
   var questionDiv = document.createElement("div");
   questionDiv.append(question);
-  for (var i = 0; i < questions[counter].a.length; i++) {
+  for (var i = 0; i < questions[questionCounter].a.length; i++) {
     var button = document.createElement("button");
-    button.textContent = questions[counter].a[i];
-    button.setAttribute("class", "btn btn-primary");
+    button.textContent = questions[questionCounter].a[i];
+    button.setAttribute("class", "btn btn-primary answers");
     questionDiv.append(button);
   }
   cardBody.append(questionDiv);
@@ -44,3 +58,51 @@ startButton.addEventListener("click", function (event) {
   showQuestions();
   //   console.log("you clicked it");
 });
+
+document.addEventListener("click", function (event) {
+  console.log(event.target.className);
+  if (event.target.className.includes("answers")) {
+    console.log("this is the answer button");
+    console.log(event.target.textContent);
+    if (event.target.textContent === questions[questionCounter].c) {
+      cardBody.append("correct");
+      correctCounter++;
+      scoreEl++;
+    } else {
+      cardBody.append("incorrect");
+    }
+    questionCounter++;
+
+    //end game function here
+    if (questionCounter >= questions.length) {
+      scoreCard();
+    } else {
+      setTimeout(function () {
+        showQuestions();
+      }, 2000);
+    }
+  }
+
+  function timer() {
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = "Time: " + secondsLeft;
+
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+    }, 1000);
+  }
+  timer();
+  //another conditional for the submit high score
+});
+
+function scoreCard() {
+  document.innerHTML = "";
+  var scoreCardEL = document.createElement("h2");
+  scoreCardEL.textContent = "High Scores";
+  var scoreCardP = document.createElement("p");
+  scoreCardP.textContent = "Your Score Is!!!";
+  console.log("score card is calling");
+  cardBody.append(scoreCardEL, scoreCardP);
+}
